52810bb6c6f26c1068e4f0ac98512131140917c0
Shantanu Patel <133038166+shantanu1807@users.noreply.github.com>
2025-01-17 21:45:16 +0530

    Time: 0 ms (100%), Space: 17.2 MB (68.04%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int  n = matrix.size();
-        int m = matrix[0].size();
-        //priority_queue<int, vector<int>, greater<int>>minheap;
-        priority_queue<int>pq;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                pq.push(matrix[i][j]);
-                 if(pq.size()>k ){
-            pq.pop();
+//         int  n = matrix.size();
+//         int m = matrix[0].size();
+//         //priority_queue<int, vector<int>, greater<int>>minheap;
+//         priority_queue<int>pq;
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 pq.push(matrix[i][j]);
+//                  if(pq.size()>k ){
+//             pq.pop();
             
-          }
-            }
-        }
+//           }
+//             }
+//         }
          
-            return pq.top();
+//             return pq.top();
+//     }
+// };
+
+int n = matrix.size();
+int low = matrix[0][0];
+int high = matrix[n-1][n-1];
+
+while(low<high){
+    int mid = low + (high-low)/2;
+    int count  =0;
+    for(int i=0;i<n; i++){
+        count += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+
+    }
+    if(count<k){
+        low = mid+1;
+    }
+    else {
+        high = mid;
+    }
+
+}
+  return low;
     }
 };
