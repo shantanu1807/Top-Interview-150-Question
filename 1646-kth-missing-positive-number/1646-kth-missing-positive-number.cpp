dad30938e8c44682e3ede0502be26986e4b9a135
Shantanu Patel <133038166+shantanu1807@users.noreply.github.com>
2025-01-27 10:17:45 +0530

    Time: 0 ms (100%), Space: 13.4 MB (27.89%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,21 +1,43 @@
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         int count =0;
+//         int ans =0;
+//         int  i =0;
+//       int start = 1;
+//       while(count<k){
+//         if(i<arr.size() && arr[i] == start){
+//             i++;
+//         }
+//         else {
+//             count++;
+//             if(count == k){
+//                 ans =  start;
+//             }
+//         }
+//         start++;
+//       }
+//       return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int count =0;
-        int  i =0;
-      int start = 1;
-      while(count<k){
-        if(i<arr.size() && arr[i] == start){
-            i++;
-        }
-        else {
-            count++;
-            if(count == k){
-                return start;
+    int findKthPositive(vector<int>& nums, int k) {
+        int n = nums.size();
+        int  l=0;
+        int r = n-1;
+        int  mid = -1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            int kitna_missing_hai_till_mid = nums[mid] - (mid+1);
+            if(kitna_missing_hai_till_mid<k){
+                l = mid+1;
+            }
+            else{
+                r= mid-1;
             }
         }
-        start++;
-      }
-      return -1;
+      return l+k;
     }
 };
