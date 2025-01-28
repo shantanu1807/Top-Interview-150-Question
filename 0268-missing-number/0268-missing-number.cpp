806f7240ce87399a8fd0688045e4cd3d6b74c1ef
Shantanu Patel <133038166+shantanu1807@users.noreply.github.com>
2025-01-28 21:21:34 +0530

    Time: 11 ms (12.77%), Space: 21.9 MB (39.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,33 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//            int n = nums.size();
+//     int ans = n; // Assume the missing number is n initially
+
+//     for (int i = 0; i < n; i++) {
+//         ans ^= i ^ nums[i]; // Use XOR to find the missing number
+//     }
+    
+//     return ans; // Return the missing number
+//     }
+// };
+
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-           int n = nums.size();
-    int ans = n; // Assume the missing number is n initially
-
-    for (int i = 0; i < n; i++) {
-        ans ^= i ^ nums[i]; // Use XOR to find the missing number
-    }
-    
-    return ans; // Return the missing number
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        //case 1
+        if(nums[0] != 0)return 0;
+        //case 2 
+        if(nums[n-1] != n)return n;
+        for(int i =1;i<nums.size();i++){
+            if(nums[i] != i){
+            //case 3
+            return i;
+            }
+        }
+        return 0;
     }
 };
