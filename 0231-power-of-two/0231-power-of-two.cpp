8f9076c654312b2d65c13543b0675f6ac9e37553
Shantanu Patel <133038166+shantanu1807@users.noreply.github.com>
2025-02-04 11:59:31 +0530

    Time: 0 ms (100%), Space: 7.9 MB (19.99%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,13 +2,27 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n==0)return false;
-    
-        while(n%2==0){
-            n/=2;
+        if(n==1)return true;
+
+        if(n%2==1){
+            return false;
         }
-        if(n==1){
-            return true;
-        }
-        return false;
+        return isPowerOfTwo(n/2);
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if(n==0)return false;
+    
+//         while(n%2==0){
+//             n/=2;
+//         }
+//         if(n==1){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
